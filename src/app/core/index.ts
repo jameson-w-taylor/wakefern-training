b55af90b926d480b72bae001b2b9c401093635d2
Jameson Taylor <jameson@ionic.io>
2023-04-17 14:48:15 -0400

    step(7): authenticate the user

--- src/app/core/index.ts
+++ src/app/core/index.ts
@@ -1,1 +1,2 @@
 export * from './session-vault/session-vault.service';
+export * from './authentication/authentication.service';
--- src/app/core/testing.ts
+++ src/app/core/testing.ts
@@ -1,1 +1,2 @@
 export * from './session-vault/session-vault.service.mock';
+export * from './authentication/authentication.service.mock';

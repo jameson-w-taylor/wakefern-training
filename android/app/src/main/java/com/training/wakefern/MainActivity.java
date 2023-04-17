eb1074fb0f0430d84384ebd94e7e011f29102d6f
Jameson Taylor <jameson@ionic.io>
2023-04-17 10:24:06 -0400

    step(2): add platforms

---
+++ android/app/src/main/java/com/training/wakefern/MainActivity.java
@@ -0,0 +1,5 @@
+package com.training.wakefern;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'tea-taster',
+  appId: 'com.training.wakefern',
+  appName: 'Tea Taster',
   webDir: 'www',
   bundledWebRuntime: false,
 };

db08600a5822d2dab8620be2dbfcb7d05e911740
Jameson Taylor <jameson@ionic.io>
2023-04-17 11:40:55 -0400

    step(4): mock up the interface

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,12 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: 'home',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
+    path: '',
+    redirectTo: 'tea',
+    pathMatch: 'full',
   },
   {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full',
+    path: 'tea',
+    loadComponent: () => import('./tea/tea.page').then((m) => m.TeaPage),
   },
 ];
